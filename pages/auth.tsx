3d155897a4f3f71eb1f33c7f4134a3d3893137a0
Supakorn <supakorn0728@gmail.com>
2023-05-02 18:31:09 +0700

    boom boom

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,7 +1,13 @@
 import Input from "../components/Input";
 import { useCallback, useState } from "react";
+import axios from "axios";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 function Auth() {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
@@ -9,6 +15,28 @@ function Auth() {
   const toggleVariant = useCallback(() => {
     setVariant((currentVariant) => (currentVariant === "login" ? "register" : "login"));
   }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", { email, password, redirect: false, callbackUrl: "/" });
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post("/api/register", {
+        email,
+        name,
+        password
+      });
+      login();
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full bg-[url('/images/hero.jpg')] bg-cover bg-fixed bg-center bg-no-repeat">
@@ -47,9 +75,22 @@ function Auth() {
                 value={password}
               />
             </div>
-            <button className="mt-10 w-full rounded-md bg-red-600 py-3 text-white transition hover:bg-red-900">
+            <button
+              onClick={variant === "login" ? login : register}
+              className="mt-10 w-full rounded-md bg-red-600 py-3 text-white transition hover:bg-red-900"
+            >
               {variant === "login" ? "Login" : "Register"}
             </button>
+
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={30} />
+              </div>
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={30} />
+              </div>
+            </div>
+
             <p className="mt-12 text-neutral-500">
               {variant === "login" ? "First time using Netflix?" : "Already have an account?"}
               <span
