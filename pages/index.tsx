036eca8d8d3cc2cdd9acea28d9db3cb44749fa35
Supakorn <supakorn0728@gmail.com>
2023-05-05 13:11:22 +0700

    boom

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,36 @@
 import { NextPageContext } from "next";
 import { signOut, getSession } from "next-auth/react";
-import useCurrentUser from "@/hook/useCurrentUser";
 import Navbar from "@/components/Navbar";
+import BillBoard from "@/components/BillBoard";
+import MovieList from "@/components/MovieList";
+import useMovieList from "@/hook/useMovieList";
 
 export async function getServerSideProps(context: NextPageContext) {
-    const session = await getSession(context);
+  const session = await getSession(context);
 
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/auth",
-                permanent: false
-            }
-        };
-    }
-
+  if (!session) {
     return {
-        props: {}
+      redirect: {
+        destination: "/auth",
+        permanent: false
+      }
     };
+  }
+
+  return {
+    props: {}
+  };
 }
 
 export default function Home() {
-    const { data: user } = useCurrentUser();
-    return (
-        <>
-            <Navbar />
-        </>
-    );
+  const { data: movies = [] } = useMovieList();
+  return (
+    <>
+      <Navbar />
+      <BillBoard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+      </div>
+    </>
+  );
 }
